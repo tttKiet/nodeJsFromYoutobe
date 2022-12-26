ef9a5e9b05ba924dbf4686ee2c592564bd6cc583
BuiKiet <roiyano1utl@gmail.com>
2022-12-26 22:19:22 +0700

    #13 HOÀN THIỆN CRUD - THÊM MỚI, HIỂN THỊ, CẬP NHẬT VÀ XÓA DỮ LIỆU

--- src/controler/homeController.js
+++ src/controler/homeController.js
@@ -30,6 +30,37 @@ class HomeController {
     );
     res.redirect("/");
   }
+
+  // [delete] /delete-user/:id
+  async deleteUser(req, res, next) {
+    let userId = req.params.id;
+    await pool.execute(`delete from users where id = ?`, [userId]);
+    res.redirect("back");
+  }
+
+  // [get] /edit-user/:id
+  async getEditUser(req, res, next) {
+    let userId = req.params.id;
+    const [user] = await pool.execute(`select * from users where id =?`, [
+      userId,
+    ]);
+
+    res.render("update", { dataUser: user[0], id: userId });
+  }
+
+  // [post] /update-user/:id
+  async updateUser(req, res, next) {
+    let userId = req.params.id;
+    const { firstName, lastName, email, address } = req.body;
+
+    await pool.execute(
+      `update users set users.firstName = ?, users.lastName = ?, users.email = ?, users.address = ? 
+      where users.id = ?`,
+      [firstName, lastName, email, address, userId]
+    );
+
+    res.redirect("/");
+  }
 }
 
 export default new HomeController();
--- src/route/web.js
+++ src/route/web.js
@@ -5,9 +5,12 @@ const router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomePage);
+  router.get("/edit-user/:id", homeController.getEditUser);
+  router.post("/update-user/:id", homeController.updateUser);
 
   router.get("/detail/user/:id", homeController.getDetailPage);
   router.post("/create-new-user", homeController.createNewUser);
+  router.post("/delete-user/:id", homeController.deleteUser);
 
   router.get("/about", (req, res) => {
     res.send("about");
